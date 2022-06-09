6661fc43c4b50c73f3c01b32b2846deaf5f48444
sunphil87 <sunphil87@naver.com>
2022-06-09 09:24:20 +0900

    21일차 종료

--- day19/js/major.js
+++
@@ -1,7 +0,0 @@
-// var selectMenu = document.testForm.major;
-
-function displaySelect(){
-    //select option 속성의 value로 선택
-    var selectText = document.getElementById("subj").value;
-    alert(selectText + "가 선택되었습니다.");
-}
--- day20/js/validation.js
+++ day20/js/validation.js
@@ -1,29 +1,35 @@
-var id = document.getElementById("user-id");
-var pw1 = document.getElementById("user-pw1");
-var pw2 = document.getElementById("user-pw2");
+//유효성 검사(체크)
+let id = document.getElementById("user-id");
+let pw1 = document.getElementById("pwd1");
+let pw2 = document.getElementById("pwd2");
 
-id.onchange = checkId;  // 함수 호출 - '()' 생략해서 이벤트 발생시에만 호출 됨
-
+//onchange 속성 사용
+id.onchange = checkId;  
+//함수 호출(괄호를 생략해서 이벤트 발생시만 호출됨)
 pw1.onchange = checkPw;
 pw2.onchange = comparePw;
 
+//아이디 체크 함수
 function checkId(){
     if(id.value.length < 4 || id.value.length > 15){
-        alert("4~15자리의 영문과 숫자로 입력해 주세요");
+        alert("아이디는 4자 이상 15자 이하로 입력하세요");
         id.select();
     }
 }
+
+//비밀번호 체크 함수
 function checkPw(){
     if(pw1.value.length < 8){
-        alert("비밀번호는 8자리 이상이어야 합니다.");
-        pw1.value = "";
-        pw1.focus();
+        alert("비밀번호는 8자 이상 입력하세요");
+        pw1.select();
     }
 }
+
+//비밀번호 일치 여부 함수
 function comparePw(){
     if(pw1.value != pw2.value){
         alert("비밀번호가 일치하지 않습니다.");
-        pw2.value = "";
-        pw2.focus();
+        pw2.value = "";   //비밀번호 초기화
+        pw2.focus();      //커서 위치
     }
 }
